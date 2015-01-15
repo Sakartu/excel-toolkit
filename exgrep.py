fdee121f435128ada3065e2edc08b4ae6edde2d3
Peter Wagenaar <sakartu@gmail.com>
2015-01-15 16:17:51 +0100

    Add support for single row checking

--- exgrep.py
+++ exgrep.py
@@ -8,6 +8,7 @@ Options:
 TERM        The term to grep for. Can be any valid (python) regular expression.
 EXCEL_FILE  The list of files to search through
 -c COL      Only search in the column specified by COL.
+-r ROW      Only search in the row specified by ROW
 -o          Only output the matched part
 """
 import re
@@ -24,16 +25,28 @@ def main():
     for f in args['EXCEL_FILE']:
         workbook = xlrd.open_workbook(f)
         sheet = workbook.sheet_by_index(0)
+
+        if args['-c']:
+            check_row(args, p, sheet, int(args['-c']))
+            continue
+
         for rownum in range(sheet.nrows):
-            for idx, v in enumerate(sheet.row_values(rownum)):
-                if args['-c'] and idx != int(args['-c']):
-                    continue
-                s = p.search(str(v))
-                if s:
-                    if args['-o']:
-                        print(s.group(0))
-                    else:
-                        print(sheet.row_values(rownum))
+            check_row(args, p, sheet, rownum)
+
+
+def check_row(args, p, sheet, rownum):
+    """
+    Check a row for the presence of pattern p.
+    """
+    for idx, v in enumerate(sheet.row_values(rownum)):
+        if args['-c'] and idx != int(args['-c']):
+            continue
+        s = p.search(str(v))
+        if s:
+            if args['-o']:
+                print(s.group(0))
+            else:
+                print(sheet.row_values(rownum))
 
 
 if __name__ == '__main__':
