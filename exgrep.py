15afb89d9e531b7754959e436663f81f3e0d6500
Peter Wagenaar <sakartu@gmail.com>
2015-01-15 16:05:04 +0100

    Add support for -o

--- exgrep.py
+++ exgrep.py
@@ -1,12 +1,13 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # -*- coding: utf8 -*-
 """
 Usage:
-exgrep TERM EXCEL_FILE...
+exgrep TERM [options] EXCEL_FILE...
 
 Options:
 TERM        The term to grep for. Can be any valid (python) regular expression.
 EXCEL_FILE  The list of files to search through
+-o          Only output the matched part
 """
 import re
 
@@ -18,14 +19,18 @@ __author__ = 'peter'
 
 def main():
     args = docopt(__doc__)
-    p = re.compile(args['TERM'])
+    p = re.compile(args['TERM'], re.UNICODE)
     for f in args['EXCEL_FILE']:
         workbook = xlrd.open_workbook(f)
         sheet = workbook.sheet_by_index(0)
         for rownum in range(sheet.nrows):
             for v in sheet.row_values(rownum):
-                if p.search(str(v)):
-                    print(sheet.row_values(rownum))
+                s = p.search(str(v))
+                if s:
+                    if args['-o']:
+                        print(s.group(0))
+                    else:
+                        print(sheet.row_values(rownum))
 
 
 if __name__ == '__main__':
